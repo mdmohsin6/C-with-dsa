c7e70452c93987d0864f459db1c95e1a1927a9bd
Md Mohsin <108623805+mdmohsin6@users.noreply.github.com>
2024-08-16 20:42:32 +0530

    Add files via upload

---
+++ sorting.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void sortArray(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        swap(arr[minIndex], arr[i]);
+    }
+}
+
+int main() {
+    int arr[5] = {3, 7, 1, 8, 9};
+    printArray(arr, 5);
+    sortArray(arr, 5);
+    cout << "Sorted array: ";
+    printArray(arr, 5);
+
+    return 0;
+}

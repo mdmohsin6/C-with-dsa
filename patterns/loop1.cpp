c4fd0dcc09a70126ff493f42adeb478e47882091
Md Mohsin <108623805+mdmohsin6@users.noreply.github.com>
2024-09-07 13:36:47 +0530

    Add files via upload

---
+++ patterns/loop1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n=4;
+    //outer lop count lines
+for(int i=1;i<=n;i++){
+    //inner loop singe row or kaam perform k leaye
+    
+    for(int j=1;j<=n;j++){
+
+        cout<<j;
+
+    }
+    cout<<endl;
+}
+return 0;
+
+}
+

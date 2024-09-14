e40e12302812ca4f8f199644620371b99b230430
Md Mohsin <108623805+mdmohsin6@users.noreply.github.com>
2024-09-14 23:02:40 +0530

    Add files via upload

---
+++ APNA COLLEGE C++/arr.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<limits.h>
+using namespace std;
+
+
+int main(){
+
+int nums[]={3,6,1,9,-2,5};
+int size=6;
+int smallest= INT_MAX:
+for(int i=0; i<size;i++){
+// if(num[i]<smallest){
+//     smallest=nums[i];
+// }
+
+smallest= min(num[i], smallest);
+}
+cout<<"smallest is-"<<smallest<<endl;
+    return 0;
+}
